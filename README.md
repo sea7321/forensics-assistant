b1aa6bb61acc96f5f8a295bf55d93b858b282915
sea2985 <savannah7321@gmail.com>
2024-04-17 16:24:15 -0400

    Adding program files.

--- README.md
+++ README.md
@@ -1,1 +1,65 @@
-# forensics_assistant
+# Forensics Assistant
+![alt assistant_example](resources/example.png)
+
+## Description
+forensics_assistant.py aids in the forensic investigation process by utilizing Google's Gemini-Pro model to read chat 
+logs from a forensic investigation and extract relevant information to an output file. This can greatly improve the 
+efficiency and accuracy of analyzing evidence.
+
+## Project Structure
+```
+Malware Detection
+│   README.md
+│   ...
+│
+└───data
+│   │   financial_chat_log.txt
+│   │   PII_chat_log.txt
+│
+└───src
+    │   config.json
+    │   forensics_assistant.py
+    │   report.txt*
+    │   requirements.txt
+    │   
+```
+
+## Getting Started
+
+### Dependencies
+* Python3
+* Colorama 
+* Termcolor
+
+### Installing
+```
+# Clone this repository
+$ git clone https://github.com/sea7321/forensics-assistant.git
+
+# Go into the repository
+$ cd forensics-assistant/src
+
+# Install dependencies
+$ pip install -r requirements.txt
+```
+
+### Obtaining an API Key
+To run the program, you need to create a config.json file and replace the api_key variable with your own Gemini API Key 
+from Google AI Studio: https://ai.google.dev/
+
+```
+{
+    "api_key": "your_api_key_here"
+}
+```
+
+### Executing program
+
+```
+# Run the forensics assistant
+$ python3 forensics_assistant.py <input_filename> <output_filename>
+```
+
+
+## Author
+* Savannah Alfaro, sea2985@rit.edu
---
+++ src/forensics_assistant.py
@@ -0,0 +1,86 @@
+"""
+File: forensics_assistant.py
+Author: Savannah Alfaro, sea2985@rit.edu
+"""
+
+# Standard Imports
+import sys
+import json
+
+# Third Party Imports
+from colorama import init
+from termcolor import colored
+import google.generativeai as genai
+
+# Gemini API Key from the config file
+with open('config.json') as f:
+    config = json.load(f)
+API_KEY = config['api_key']
+
+
+def read_file(filename):
+    """
+    Reads an input file and returns the file content.
+    :param filename: (String) the input filename
+    :return: (String[]) the file content
+    """
+    # read content from the input file
+    print(colored("[*] Reading content from {}".format(filename), "cyan"))
+    try:
+        with open(filename) as file:
+            return file.readlines()
+    except Exception as e:
+        print(colored("\tUnable to read content from {}.\n\t{}".format(filename, e), "red"))
+
+
+def call_gemini(content):
+    """
+    Calls the Gemini-Pro model and returns the model's response.
+    :param content: (String []) the file content
+    :return: (String) the model's response
+    """
+    # configure the model and response
+    print(colored("[*] Configuring the Gemini-Pro model", "cyan"))
+    try:
+        genai.configure(api_key=API_KEY)
+        model = genai.GenerativeModel("gemini-pro")
+        return model.generate_content("Below is a chat log from a forensic investigation. Please extract any "
+                                      "necessary information for a forensic report and provide a brief summary: {}"
+                                      .format(content))
+    except Exception as e:
+        print(colored("\tFailed to retrieve a response from the model.\n\t{}".format(e), "red"))
+
+
+def write_report(response, filename):
+    """
+    Writes the model's response to an output file.
+    :param response: (String) the model's response
+    :param filename: (String) the output filename
+    :return: None
+    """
+    # write response to an output file
+    print(colored("[*] Writing the response to {}".format(filename), "cyan"))
+    try:
+        with open(output_filename, 'w') as file:
+            file.writelines(response.text)
+        print("\tSuccessfully wrote {} lines to {}".format(len(response.text), filename))
+    except Exception as e:
+        print(colored("\tUnable to write content to {}.\n\t{}".format(filename, e), "red"))
+
+
+if __name__ == "__main__":
+    # initialize colorama
+    init()
+
+    # command line arguments
+    input_filename = sys.argv[1]
+    output_filename = sys.argv[2]
+
+    # read the file content
+    file_content = read_file(input_filename)
+
+    # call gemini-pro
+    gemini_response = call_gemini(file_content)
+
+    # construct the forensic report
+    write_report(gemini_response, output_filename)
